4dbfc46c673959044658fcc0332124e16ce345d8
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2021-12-31 03:32:24 +0100

    feat: more logging

--- index.js
+++ index.js
@@ -16,14 +16,16 @@ try {
     const cfProjectName = core.getInput('cf-project-name');
     const deployments_endpoint_base = 'https://api.cloudflare.com/client/v4/accounts/' + cfAccountID + '/pages/projects/' + cfProjectName + '/deployments';
 
+    console.log('URL', deployments_endpoint_base);
+
     const {result} = got.get(deployments_endpoint_base, request).json();
 
     forEach(result, (function (deploy) {
         console.log(deploy.id, deploy.environment, deploy.deployment_trigger.metadata.branch);
     }));
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
