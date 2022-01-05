1e37fd7ae027f66741bfdd3954a9eba1fbeb6e4f
Łukasz Rżanek <lukasz.rzanek@radical.com.pl>
2022-01-05 09:54:02 +0100

    feat: branch

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 import * as github from "@actions/github";
 import core from '@actions/core';
-import got from 'got';
-import _ from "lodash";
+import {CloudflareDeleteDeployments} from "./cloudflare.js";
 
 try {
-    const request = {
-        headers: {
-            "content-type": "application/json;charset=UTF-8",
-            'Accept': "application/json",
-            "X-Auth-Email": core.getInput("cf-email"),
-            "X-Auth-Key": core.getInput('cf-token'),
-        },
-    }
+    const cfEmail = core.getInput('cf-email');
+    const cfToken = core.getInput('cf-token');
     const cfAccountID = core.getInput('cf-account-id');
     const cfProjectName = core.getInput('cf-project-name');
-    const deployments_endpoint_base = 'https://api.cloudflare.com/client/v4/accounts/' + cfAccountID + '/pages/projects/' + cfProjectName + '/deployments';
 
-    const {result} = await got.get(deployments_endpoint_base, request).json();
-
-    _.forEach(result, (function (deploy) {
-        console.log(deploy.id, deploy.environment, deploy.deployment_trigger.metadata.branch);
-    }));
+    await CloudflareDeleteDeployments(cfEmail, cfToken, cfAccountID, cfProjectName);
 
     const payload = JSON.stringify(github.context.ref, undefined, 2)
     console.log(`The event payload: ${payload}`);
